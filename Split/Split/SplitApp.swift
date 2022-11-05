0d588b124aae9edce01f7a90900cc6cd4074e683
AnthonyLees <alees@andrew.cmu.edu>
2022-11-04 21:40:02 -0400

    Initial Setup of Files

---
+++ Split/Split/SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitApp.swift
+//  Split
+//
+//  Created by Anthony A Lees on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
