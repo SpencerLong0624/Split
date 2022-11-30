037b5f0d04f3de78c7828e7f4ca975626d508c52
Spencer Long <spencerlong0624@gmail.com>
2022-11-30 16:14:46 -0500

    Tried ways to fix edit button

--- Split/Split/Views/EditItem.swift
+++ Split/Split/Views/EditItem.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct EditItem: View {
+  
+  var billTitle: String
+  var billDescription: String
+  var billDate: String
+  @State var curItem : BillItem
+  
+  @State var billItemsObject : BillItems = BillItems()
+    
+  @State private var itemName: String = ""
+  @State private var itemPrice: String = ""
+  
+  func changeItem() {
+    curItem.name = itemName
+    curItem.price = itemPrice
+  }
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+      VStack {
+        HStack {
+          Text(billTitle)
+            .font(.title)
+            .frame(maxWidth: .infinity, alignment: .leading)
+          Text(billDate)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+          Spacer()
+            .frame(width: 10)
+        }
+        Text(billDescription)
+          .frame(maxWidth: .infinity, alignment: .leading)
+        Divider()
+          .frame(width: 360, height: 1)
+          .overlay(.black)
+        Form {
+          Section(header: Text("Item Information")){
+            TextField("Enter a Title: ", text: $itemName)
+            TextField("Enter a price: $", text: $itemPrice)
+          }
 
-struct EditItem_Previews: PreviewProvider {
-    static var previews: some View {
-        EditItem()
+          Section(){
+            NavigationLink(destination: Manually_AddItem(billTitle: billTitle, billDescription: billDescription, billDate: billDate, billItems: billItemsObject)) {
+              Text("Add Item")
+             }
+            .onTapGesture {
+               changeItem()
+           }
+          }
+        }
+      }
     }
 }
