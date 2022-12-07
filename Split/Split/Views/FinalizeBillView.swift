0530ca32197e6023fa45c0f6f9a1443705cca822
Spencer Long <spencerlong0624@gmail.com>
2022-12-07 00:24:49 -0500

    new finalize bill view

---
+++ Split/Split/Views/FinalizeBillView.swift
@@ -0,0 +1,20 @@
+//
+//  FinalizeBillView.swift
+//  Split
+//
+//  Created by Spencer Long on 12/7/22.
+//
+
+import SwiftUI
+
+struct FinalizeBillView: View {
+    var body: some View {
+        Text("Finalize Bill View")
+    }
+}
+
+struct FinalizeBillView_Previews: PreviewProvider {
+    static var previews: some View {
+        FinalizeBillView()
+    }
+}
