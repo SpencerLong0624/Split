170387789ca85ab5cd46416b0e15454e561d48dc
AnthonyLees <alees@andrew.cmu.edu>
2022-12-08 23:20:27 -0500

    Fix search bug when adding friends

--- Split/Split/Views/FriendsView.swift
+++ Split/Split/Views/FriendsView.swift
@@ -11,7 +11,6 @@ struct FriendsView: View {
   @ObservedObject var usersViewModel = UsersViewModel()
   @EnvironmentObject private var authModel: AuthViewModel
   @State var searchField: String = ""
-  @State var displayedUsers : [UserViewModel] = []
   
   var body: some View {
     let binding = Binding<String>(get: {
@@ -19,7 +18,6 @@ struct FriendsView: View {
     }, set: {
       self.searchField = $0
       self.usersViewModel.search(searchText: self.searchField)
-      self.displayBills()
     })
     
     NavigationStack {
@@ -27,7 +25,7 @@ struct FriendsView: View {
         TextField("Search for a friend with their email", text: binding)
           .tint(.black)
         List {
-          ForEach(displayedUsers) { userViewModel in
+          ForEach(displayUsers()) { userViewModel in
             UserRowView(curr_user: usersViewModel.getUser(email: authModel.user?.email ?? "")[0].user, friend_user: userViewModel.user)
           }
         }
@@ -37,25 +35,19 @@ struct FriendsView: View {
     .navigationBarColor(UIColor(red: 76/255, green: 229/255, blue: 177/255, alpha: 255/255))
   }
   
-  func loadData() {
-    self.displayedUsers = self.usersViewModel.userViewModels
-    self.displayedUsers = self.displayedUsers.filter {
-      $0.user.email != authModel.user?.email!
-    }
-  }
-  
-  func displayBills() {
+  func displayUsers() -> [UserViewModel] {
+    var displayedUsers : [UserViewModel] = []
     if searchField == "" {
-      self.displayedUsers = self.usersViewModel.userViewModels
-      self.displayedUsers = self.displayedUsers.filter {
+      displayedUsers = self.usersViewModel.userViewModels.filter {
         $0.user.email != authModel.user?.email!
       }
     } else {
-      self.displayedUsers = self.usersViewModel.filteredUserViewModels
-      self.displayedUsers = self.displayedUsers.filter {
+      displayedUsers = self.usersViewModel.filteredUserViewModels.filter {
         $0.user.email != authModel.user?.email!
       }
     }
+    
+    return displayedUsers
   }
 }
 
