06f470afd314ea207adf1e1638365e7b62b39fde
Spencer Long <spencerlong0624@gmail.com>
2022-12-14 02:17:56 -0500

    changed account view ui

--- Split/Split/Views/AccountView.swift
+++ Split/Split/Views/AccountView.swift
@@ -18,83 +18,85 @@ struct AccountView: View {
 
     var body: some View {
         NavigationStack{
-            VStack{
-                Text("EMAIL")
-                    .font(.system(size: 20))
-                    .multilineTextAlignment(.leading)
-                Form{
-                    Text("\(authModel.user?.email ?? "")")
-                        .font(.system(size: 20))
-                }
-                Text("FULL NAME")
-                Form{
-                    HStack {
-                        if nameInEditMode {
-                            TextField("Enter your Full Name", text: $full_name)
-                                .tint(.green)
-                                .textFieldStyle(RoundedBorderTextFieldStyle()).padding(.leading, 4).font(.system(size: 20))
-                                .autocapitalization(.words)
-                                .disableAutocorrection(true)
-                        } else {
-                            Text("\(usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user.full_name)").font(.system(size: 20))
-                        }
-                        
-                        Button(action: {
-                            self.nameInEditMode.toggle()
-                            var user : User = usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user
-                            user.full_name = full_name
-                            usersViewModel.userRepository.update(user)
-                        }) {
-                            Image(systemName: "pencil")
-                                .foregroundColor(Color.blue)
-                            Text(nameInEditMode ? "Done" : "Edit").font(.system(size: 20)).fontWeight(.light)
-                                .foregroundColor(Color.blue)
-                        }
+          VStack {
+            Form {
+              Section(header: Text("Email")) {
+                Text("\(authModel.user?.email ?? "")")
+              }
+              
+              Section(header: Text("Full Name")) {
+                HStack {
+                  if nameInEditMode {
+                    TextField("Enter your Full Name", text: $full_name)
+                      .tint(.gray)
+                      .autocapitalization(.words)
+                      .disableAutocorrection(true)
+                      .frame(maxWidth: .infinity, alignment: .leading)
+                    } else {
+                      Text("\(usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user.full_name)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
+                  
+                  Button(action: {
+                    self.nameInEditMode.toggle()
+                    var user : User = usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user
+                    user.full_name = full_name
+                    usersViewModel.userRepository.update(user)
+                  }, label: {
+                    Image(systemName: "pencil")
+                      .foregroundColor(Color.blue)
+                    Text(nameInEditMode ? "Done" : "Edit")
+                      .foregroundColor(Color.blue)
+                  })
+                  .buttonStyle(BorderlessButtonStyle())
+                  .frame(maxWidth: .infinity, alignment: .trailing)
                 }
-                Text("PHONE NUMBER")
-                    .font(.system(size: 20))
-                Form{
-                    HStack {
-                        if phoneInEditMode {
-                            TextField("Enter your Phone Number", text: $phone_number)
-                                .tint(.green)
-                                .textFieldStyle(RoundedBorderTextFieldStyle()).padding(.leading, 5).font(.system(size: 20))
-                                .autocapitalization(.words)
-                                .disableAutocorrection(true)
-                        } else {
-                            Text("\(usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user.phone_number)").font(.system(size: 20))
-                        }
-                        
-                        Button(action: {
-                            self.phoneInEditMode.toggle()
-                            var user : User = usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user
-                            user.phone_number = phone_number
-                            usersViewModel.userRepository.update(user)
-                        }) {
-                            Image(systemName: "pencil")
-                                .foregroundColor(Color.blue)
-                            Text(phoneInEditMode ? "Done" : "Edit").font(.system(size: 20)).fontWeight(.light)
-                                .foregroundColor(Color.blue)
-                        }
+              }
+              
+              Section(header: Text("Phone Number")) {
+                HStack {
+                    if phoneInEditMode {
+                      TextField("Enter your Phone Number", text: $phone_number)
+                        .tint(.gray)
+                        .autocapitalization(.words)
+                        .disableAutocorrection(true)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    } else {
+                        Text("\(usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user.phone_number)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     }
+                  
+                  Button(action: {
+                    self.phoneInEditMode.toggle()
+                    var user : User = usersViewModel.getUser(email: authModel.user?.email ?? "anthony@gmail.com")[0].user
+                    user.phone_number = phone_number
+                    usersViewModel.userRepository.update(user)
+                  }, label: {
+                    Image(systemName: "pencil")
+                      .foregroundColor(Color.blue)
+                    Text(phoneInEditMode ? "Done" : "Edit")
+                      .foregroundColor(Color.blue)
+                  })
+                  .buttonStyle(BorderlessButtonStyle())
+                  .frame(maxWidth: .infinity, alignment: .trailing)
                 }
-               
+              }
+              
+              Section {
                 Button(
-                    action: { authModel.signOut()
-                    }, label: {
-                        Text("Sign Out") .bold().accentColor(.red)
-                    })
-            }.navigationTitle("My Account")
-            .navigationBarColor(UIColor(red: 76/255, green: 229/255, blue: 177/255, alpha: 255/255), UIColor.white)
-            .background(Color(red: 0.949, green: 0.949, blue: 0.97, opacity: 1.0))
-
+                  action: { authModel.signOut()
+                }, label: {
+                  Text("Sign Out")
+                    .bold()
+                    .accentColor(.red)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                })
+              }
+            }
+          }
+          .navigationTitle("My Account")
+          .navigationBarColor(UIColor(red: 76/255, green: 229/255, blue: 177/255, alpha: 255/255), UIColor.white)
+          .background(Color(red: 0.949, green: 0.949, blue: 0.97, opacity: 1.0))
         }
     }
-}
-
-struct MainView_Previews: PreviewProvider {
-  static var previews: some View {
-    AccountView()
-  }
 }
